5e7d2a3a2911cff7819790ad7dbe7839d34ed733
Jan Dudek <jd@jandudek.com>
2014-10-30 15:54:47 +0100

    Initial commit

---
+++ system-check.sh
@@ -0,0 +1,32 @@
+function check {
+  msg=$1
+  shift
+  eval "$@" >/dev/null 2>&1
+
+  if [ $? -eq 0 ]
+  then
+    echo "[✔] $msg" >&2
+  else
+    echo "[ ] $msg" >&2
+    failed=1
+  fi
+}
+
+failed=0
+
+check "GCC installed"             'gcc --version'
+check "Git installed"             'git --version'
+check "Ruby 2.1.4 installed"      '(ruby -v 2>/dev/null | grep "2.1.4")'
+check "Bundler installed"         '(gem list | grep "bundler")'
+check "PostgreSQL installed"      'which psql'
+check "pg gem installed"          '(gem list | grep "pg")'
+check "SQLite installed"          'which sqlite3'
+check "sqlite3 gem installed"     '(gem list | grep "sqlite3")'
+check "nokogiri gem installed"    '(gem list | grep "nokogiri")'
+check "Node.js installed"         'node -v'
+check "Heroku Toolbelt installed" 'which heroku'
+
+if [ $failed -eq 0 ]
+then
+  bundle install && echo "Everything is working!"
+fi
